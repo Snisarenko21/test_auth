e6447b3fcd737594e9b444ab83bab92809274b2b
Snisarenko21 <sepalena@gmail.com>
2022-10-22 22:48:45 +0300

    4

--- controllers/user/register.js
+++ controllers/user/register.js
@@ -1,6 +1,5 @@
 const User = require("../../models/contacts/user");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -9,14 +8,15 @@ const register = async (req, res) => {
   const hashPass = await bcrypt.hash(password, 10);
 
   const user = await User.create({ name, email, password: hashPass });
-
-  const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
-  await User.findByIdAndUpdate(user._id, { token });
+  if (user) {
+    res.status(403).json({ message: "Доступ заборонено" });
+  }
+  // const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
+  // await User.findByIdAndUpdate(user._id, { token });
 
   res.status(201).json({
     status: "success",
     code: 201,
-    token,
     user: { name, email },
   });
 };
