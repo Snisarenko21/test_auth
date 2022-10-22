d11aa6276f90c8297ed803a40a4ef8383041b26b
Snisarenko21 <sepalena@gmail.com>
2022-10-22 20:30:23 +0300

    2

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const { DB_HOST, PORT = 3001 } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(PORT))
+  .then(() => app.listen(PORT || 3001))
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
